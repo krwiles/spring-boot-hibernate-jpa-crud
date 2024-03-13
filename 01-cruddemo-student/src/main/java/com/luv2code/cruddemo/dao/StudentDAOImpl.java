827e71d341b0a9ec21af622daa340bafbbed53f9
Kurtis Wiles <kurtis.r.wiles@gmail.com>
2024-03-12 20:55:51 -0400

    Creates student dao

---
+++ 01-cruddemo-student/src/main/java/com/luv2code/cruddemo/dao/StudentDAOImpl.java
@@ -0,0 +1,24 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+
+    // define field for entity manager
+    private EntityManager entityManager;
+
+    // inject using constructor injection
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+}
